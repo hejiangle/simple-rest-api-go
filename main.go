2832e91522f5fb4f99a301aa106823112fe2f593
hejiangle <jlhe@thoughtworks.com>
2020-05-18 17:59:27 +0800

    Add gorilla mux module to split serverhttp method to different methods.

--- main.go
+++ main.go
@@ -1,33 +1,52 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func get(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/type")
-	switch r.Method {
-	case http.MethodGet:
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message: "get called"}`))
-	case http.MethodPost:
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message: "post called}`))
-	case http.MethodPut:
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message": "put called"}`))
-	case http.MethodDelete:
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "delete called"}`))
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message": "not found"}`))
-	}
 
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message: "get called"}`))
+}
+
+func post(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/type")
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message: "post called}`))
+}
+
+func put(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/type")
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(`{"message": "put called"}`))
+}
+
+func delete(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/type")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "delete called"}`))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/type")
+
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"message": "not found"}`))
 }
 
 func main() {
-	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", get).Methods(http.MethodGet)
+	r.HandleFunc("/", post).Methods(http.MethodPost)
+	r.HandleFunc("/", put).Methods(http.MethodPut)
+	r.HandleFunc("/", delete).Methods(http.MethodDelete)
+	r.HandleFunc("/", notFound)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
