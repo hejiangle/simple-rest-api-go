e4e2b7a7949ce77f7cb892655d88ba6df86b0472
hejiangle <jlhe@thoughtworks.com>
2020-05-18 17:24:30 +0800

    Add different method handle ways of Get, POST, DELETE, and PUT.

--- main.go
+++ main.go
@@ -11,8 +11,24 @@ type server struct {
 
 func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/type")
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write([]byte(`{"message: "hello, world!"}`))
+	switch r.Method {
+	case http.MethodGet:
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message: "get called"}`))
+	case http.MethodPost:
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message: "post called}`))
+	case http.MethodPut:
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte(`{"message": "put called"}`))
+	case http.MethodDelete:
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "delete called"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "not found"}`))
+	}
+
 }
 
 func main() {
