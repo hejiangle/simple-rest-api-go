0d9102c14ce4ee22361889cdeba237427ad3bf16
hejiangle <jlhe@thoughtworks.com>
2020-05-18 17:44:24 +0800

    remove the server and using handleFunc to handle request.

--- main.go
+++ main.go
@@ -5,11 +5,7 @@ import (
 	"net/http"
 )
 
-type server struct {
-
-}
-
-func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/type")
 	switch r.Method {
 	case http.MethodGet:
@@ -32,7 +28,6 @@ func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	s := server{}
-	http.Handle("/", s)
+	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
