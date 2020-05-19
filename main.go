410129be4168648115c49336adc1cfb90297287d
hejiangle <jlhe@thoughtworks.com>
2020-05-19 11:22:52 +0800

    Add one more user api to display path parameters and query paraments.

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func get(w http.ResponseWriter, r *http.Request){
@@ -41,6 +43,37 @@ func notFound(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte(`{"message": "not found"}`))
 }
 
+func params(w http.ResponseWriter, r *http.Request){
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/type")
+
+	userID := -1
+	var err error
+	if value, ok := pathParams["userID"]; ok {
+		userID, err = strconv.Atoi(value)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+	}
+
+	commentID := -1
+	if value, ok := pathParams["commentID"]; ok {
+		commentID, err = strconv.Atoi(value)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"message": "need a number"}`))
+			return
+		}
+	}
+
+	query := r.URL.Query()
+	location := query.Get("location")
+
+	w.Write([]byte(fmt.Sprintf(`{"userID"": %d, "CommentID": %d, "location": %s }`, userID, commentID, location)))
+}
+
 func main() {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
@@ -49,5 +82,8 @@ func main() {
 	api.HandleFunc("/", put).Methods(http.MethodPut)
 	api.HandleFunc("/", delete).Methods(http.MethodDelete)
 	api.HandleFunc("/", notFound)
+
+	api.HandleFunc("/user/{userID}/comment/{commentID}", params).Methods(http.MethodGet)
+
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
