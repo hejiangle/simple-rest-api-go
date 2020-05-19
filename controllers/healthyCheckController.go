d30dc0547865b14de32379a8ae21cde672252b53
hejiangle <jlhe@thoughtworks.com>
2020-05-19 13:48:08 +0800

    Add the healthy check api.

--- controllers/healthyCheckController.go
+++ controllers/healthyCheckController.go
@@ -1,1 +1,10 @@
 package controllers
+
+import "net/http"
+
+func Healthy(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/type")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "Welcome to simple api demo", "version": "1.0"}`))
+}
